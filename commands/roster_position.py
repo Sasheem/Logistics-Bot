82be8a15e672ab7cfaa29bf3edf7a57eeb55f53e
Sasheem <sasheem.snell@gmail.com>
2024-12-31 12:37:42 -0500

    Normalize names and soften the name matching

--- commands/roster_position.py
+++ commands/roster_position.py
@@ -1,28 +1,37 @@
-# commands/roster_position.py
-
 from interactions import CommandContext
 from rapidfuzz import process, fuzz
 from config.google_sheets import client_gs
 from config.constants import WAR_SHEET_ID
 from utils.fetch_roster_info import fetch_roster_info
 from utils.fetch_data_with_cache import fetch_data_with_cache
+import re
+
+def normalize_string(s):
+    if not isinstance(s, str):
+        return ''
+    return re.sub(r'\W+', '', s).lower()
 
 async def roster_position(ctx: CommandContext, name: str, clear_cache: bool = False):
     await ctx.defer()  # Defer the interaction to give more time
     spreadsheet_id = WAR_SHEET_ID
     sheet_names = ['WHSKY', 'TANGO', 'FXTRT']
 
+    # Normalize the input name
+    normalized_name = normalize_string(name)
+
     # Fetch data with or without cache based on clear_cache parameter
-    roster_info = fetch_roster_info(client_gs, spreadsheet_id, str(name).strip(), use_cache=not clear_cache)
+    roster_info = fetch_roster_info(client_gs, spreadsheet_id, normalized_name, use_cache=not clear_cache)
 
     # Soft match if no exact match found
     if not roster_info or not roster_info["T1"]:
         for sheet_name in sheet_names:
             data = fetch_data_with_cache(client_gs, spreadsheet_id, sheet_name, use_cache=not clear_cache)
-            soft_matches = process.extract(str(name), [str(entry[f'Name {sheet_name}']) for entry in data], scorer=fuzz.token_sort_ratio)
+            normalized_data = [normalize_string(entry[f'Name {sheet_name}']) for entry in data]
+            soft_matches = process.extract(normalized_name, normalized_data, scorer=fuzz.partial_ratio)
             best_match = soft_matches[0] if soft_matches else None
             if best_match and best_match[1] > 70:
-                roster_info = fetch_roster_info(client_gs, spreadsheet_id, best_match[0], use_cache=not clear_cache)
+                original_name = next(entry[f'Name {sheet_name}'] for entry in data if normalize_string(entry[f'Name {sheet_name}']) == best_match[0])
+                roster_info = fetch_roster_info(client_gs, spreadsheet_id, original_name, use_cache=not clear_cache)
                 break
 
     if roster_info and roster_info["T1"]:
@@ -43,4 +52,4 @@ async def roster_position(ctx: CommandContext, name: str, clear_cache: bool = Fa
             print(f"Error sending response: {e}")
             await ctx.send(f"An error occurred while sending the response for {name}. Please try again.")
     else:
-        await ctx.send(f"## Oops, this is embarrassing..\n\nPlayer not found: **{name}**.\nPlease check the spelling and try again.")
+        await ctx.send(f"**Oops, this is embarrassing..**\n\nPlayer not found: **{name}**.\nPlease check the spelling and try again.")
